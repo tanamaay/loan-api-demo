4b59dff3b01854de54265556cbb8df23ee7bb336
Tanamay Patel <98833259+tanamaay@users.noreply.github.com>
2025-12-29 23:09:42 +0530

    Enhance ExternalLoanResponseDto with EMI details
    Updated ExternalLoanResponseDto to include EMI details.

--- loan-api-demo/src/main/java/com/example/loan_api_demo/dto/ExternalLoanResponseDto.java
+++ loan-api-demo/src/main/java/com/example/loan_api_demo/dto/ExternalLoanResponseDto.java
@@ -1,10 +1,34 @@
 package com.example.loan_api_demo.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ExternalLoanResponseDto {
+
+    @JsonProperty("loanAccountNumber")
     private String loanAccountNumber;
-    private String dueDate;
-    private Double emiAmount;
+
+    @JsonProperty("emiDetails")
+    private List<EmiDetail> emiDetails;
+
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class EmiDetail {
+        @JsonProperty("month")
+        private String month;
+
+        @JsonProperty("emiAmount")
+        private Double emiAmount;
+
+        @JsonProperty("paidStatus")
+        private Boolean paidStatus;
+
+        @JsonProperty("dueStatus")
+        private Boolean dueStatus;
+    }
 }
