0bde4c079fc9c47c2a4be1ef421792020701a64e
Tanamay Patel <98833259+tanamaay@users.noreply.github.com>
2025-12-29 23:10:17 +0530

    Update LoanIntegrationClient.java

--- loan-api-demo/src/main/java/com/example/loan_api_demo/integration/LoanIntegrationClient.java
+++ loan-api-demo/src/main/java/com/example/loan_api_demo/integration/LoanIntegrationClient.java
@@ -9,16 +9,23 @@ import org.springframework.web.client.RestTemplate;
 @Slf4j
 public class LoanIntegrationClient {
 
-    private  final RestTemplate restTemplate;
+    private final RestTemplate restTemplate;
 
-    public  LoanIntegrationClient (RestTemplate restTemplate){
+    public LoanIntegrationClient(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
 
-    public ExternalLoanResponseDto fetchLoanDetails(String  loanAccountNumber){
-        String url = "https://demo9993930.mockable.io/loanaccount/1" + loanAccountNumber;
+    public ExternalLoanResponseDto fetchLoanDetails(String loanAccountNumber) {
+        String url = "https://demo9993930.mockable.io/loanaccount/" + loanAccountNumber;
         log.info("Calling external loan API for loanAccountNumber={}", loanAccountNumber);
 
-        return restTemplate.getForObject(url,ExternalLoanResponseDto.class);
+        String rawResponse = restTemplate.getForObject(url, String.class);
+        log.info("RAW API RESPONSE: {}", rawResponse);
+
+        ExternalLoanResponseDto loanResponse =
+                restTemplate.getForObject(url, ExternalLoanResponseDto.class);
+
+        log.info("Mapped ExternalLoanResponseDto = {}", loanResponse);
+        return loanResponse;
     }
 }
